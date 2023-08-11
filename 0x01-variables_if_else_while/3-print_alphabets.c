5dee2d9fe7b6a28e429eacf4e80434d333bf445b
Nathaniel Ogbada <nathanielogbada@gmail.com>
2023-08-11 10:03:58 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,11 +9,10 @@ int main(void)
 {
 	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
-	
 	for (i = 0; i < 52; i++)
 	{
 		putchar(alp[i]);
 	}
 	putchar('\n');
-       return (0);
+	return (0);
 }
