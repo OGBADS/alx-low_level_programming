459d8827b093477e8a453430b67a125fcc2e6379
Nathaniel Ogbada <nathanielogbada@gmail.com>
2023-08-15 09:31:34 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,47 +1,48 @@
 #include "main.h"
 
 /**
- * print_times_table - prints the n times table, starting with 0 
+ * print_times_table - Prints the n times table,
+ * starting with 0 
  * @n: number of the times table
  *Return: Number matrix
  */
 void print_times_table(int n)
 {
-	int i, j, k;
+	int x, y, z;
 
 	if (n >= 0 && n <= 14)
 	{
-		for (i = 0; i <= n; i++)
+		for (x = 0; x <= n; x++)
 		{
-			for (j = 0; j <= n; j++)
+			for (y = 0; y <= n; y++)
 			{
-				k = j * i;
-				if (k > 99)
+				z = x * y;
+				if (z > 99)
 				{
 					_putchar(',');
 					_putchar(32);
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
+					_putchar((z / 100) + '0');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
 				}
-				else if (k > 9)
+				else if (z > 9)
 				{
 					_putchar(',');
 					_putchar(32);
 					_putchar(32);
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
 				}
 				else
 				{
-					if (k >= 100)
-				{
-					_putchar(',');
-					_putchar(32);
-					_putchar(32);
-					_putchar(32);
-				}
-				_putchar(k + '0');
+					if (y != 0)
+					{
+						_putchar(',');
+						_putchar(32);
+						_putchar(32);
+						_putchar(32);
+					}
+					_putchar(z + '0');
 				}
 			}
 			_putchar('\n');
