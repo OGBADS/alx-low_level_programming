8242b71fc62ff825da32c8fef7214e2265b2b161
Nathaniel Ogbada <nathanielogbada@gmail.com>
2023-08-17 13:13:51 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,7 +9,7 @@ void print_triangle(int size)
 {
 	if (size <= 0)
 	{
-		putchar('\n');
+		_putchar('\n');
 	}
 	else
 	{
@@ -19,13 +19,13 @@ void print_triangle(int size)
 		{
 			for (j = i; j < size; j++)
 			{
-				putchar(' ');
+				_putchar(' ');
 			}
 			for (j = 1; j <= i; j++)
 			{
-				putchar('#');
+				_putchar('#');
 			}
-			putchar('\n');
+			_putchar('\n');
 		}
 	}
 }
