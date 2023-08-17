d4886c7ea94a66ce5f47781de53ade828629c2df
Nathaniel Ogbada <nathanielogbada@gmail.com>
2023-08-17 13:20:02 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,7 +9,7 @@ void print_line(int n)
 {
 	if (n <= 0)
 	{
-		putchar('\n');
+		_putchar('\n');
 	}
 	else
 	{
@@ -17,8 +17,8 @@ void print_line(int n)
 
 		for (i = 1; i <= n; i++)
 		{
-			putchar('_');
+			_putchar('_');
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
 }
