9766532c960fac6909ced491f26cc0a91306d2e2
Nathaniel Ogbada <nathanielogbada@gmail.com>
2023-08-30 10:21:08 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _print_rev_recursion -  a function that prints a string in reverse.
- *
+ * @s - input
  * Return: Always 0 (success)
  */
 void _print_rev_recursion(char *s)
