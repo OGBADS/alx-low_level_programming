ab430bfa2adaf37cfd248f4b7dece780c85def3d
Nathaniel Ogbada <nathanielogbada@gmail.com>
2023-08-30 11:42:33 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -16,13 +16,14 @@ int last_index(char *s)
 	return (n);
 }
 /**
- * is_palindrome - a function that returns 1 if a string is a palindrome and 0 if not.
+ * is_palindrome - a function that returns 1 if a
+ * string is a palindrome and 0 if not.
  * @s: string to check
  * Return: 0 or 1
  */
 int is_palindrome(char *s)
 {
-	int end = last_index (s);
+	int end = last_index(s);
 
 	return (check(s, 0, end - 1, end % 2));
 }
