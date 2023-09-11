8f492526681957e4862af4279c941083478c13ec
Nathaniel Ogbada <nathanielogbada@gmail.com>
2023-09-11 08:44:27 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - prints the name of the file it was compiled from 
+ * main - prints the name of the file it was compiled from
  * Return:0
  */
 int main(void)
 {
-	printf("%s\n",__BASE_FILE__);
+	printf("%s\n", __BASE_FILE__);
 	return (0);
 }
